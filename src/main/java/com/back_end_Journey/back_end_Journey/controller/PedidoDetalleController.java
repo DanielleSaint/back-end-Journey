d63d2ce75b68df926d386c04d0aa8689c7954867
DanielleSaint <daniellesaint09@gmail.com>
2025-05-30 17:53:54 -0500

    Subiendo archivos del PedidoDetalle

---
+++ src/main/java/com/back_end_Journey/back_end_Journey/controller/PedidoDetalleController.java
@@ -0,0 +1,9 @@
+package com.back_end_Journey.back_end_Journey.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pedidos_detalle")
+public class PedidoDetalleController {
+    //Realizar esta clase
+}
