00b11ba2bca36056e5e59245a661351751742dfc
DanielleSaint <daniellesaint09@gmail.com>
2025-05-30 16:09:48 -0500

    Archivos Spring

---
+++ src/main/java/com/back_end_Journey/back_end_Journey/BackEndJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.back_end_Journey.back_end_Journey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndJourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndJourneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/back_end_Journey/back_end_Journey/BackEndJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back_end_Journey.back_end_Journey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndJourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
