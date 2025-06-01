beb5b6799bf32fb86c568dcca977654773e490ca
Carlos Mario <carlosmariobig14@gmail.com>
2025-05-31 23:45:22 -0500

    Controller TERMINADO

--- src/main/java/com/back_end_Journey/back_end_Journey/controller/ProductoController.java
+++ src/main/java/com/back_end_Journey/back_end_Journey/controller/ProductoController.java
@@ -2,10 +2,10 @@ package com.back_end_Journey.back_end_Journey.controller;
 
 import com.back_end_Journey.back_end_Journey.model.Producto;
 import com.back_end_Journey.back_end_Journey.service.ProductoService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
 
 @RestController
 @RequestMapping("/productos")
@@ -15,9 +15,32 @@ public class ProductoController {
     public ProductoController(ProductoService productoService) {
         this.productoService = productoService;
     }
-    
+
     @GetMapping("/ObtenerProductos")
     public List<Producto> listaProductos(){
         return productoService.obtenerTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Producto obtenerPorID(@PathVariable Long id){
+        return productoService.obtenerPorId(id);
+    }
+
+    @PostMapping(value = "/guardarNuevoProducto")
+    public ResponseEntity<String> guardarProducto(@RequestBody Producto producto) {
+        productoService.guardarProducto(producto);
+        return ResponseEntity.ok("Producto agregado con éxito");
+    }
+
+    @DeleteMapping("/borrar/{id}")
+    public ResponseEntity<String> eliminarProducto(@PathVariable Long id) {
+        productoService.deleteProducto(id);
+        return ResponseEntity.ok("Producto eliminado con éxito");
+    }
+
+    @PutMapping("/editar/{id}")
+    public ResponseEntity<String> editarProducto(@PathVariable Long id, @RequestBody Producto productoActualizado) {
+        productoService.updateProducto(id, productoActualizado);
+        return ResponseEntity.ok("Producto actualizado con éxito");
     }
 }
