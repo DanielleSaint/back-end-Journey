95a7b548a999c8227d05933931f8e5482a6c5eda
Carlos Mario <carlosmariobig14@gmail.com>
2025-05-31 23:31:04 -0500

    Service TERMINADO

--- src/main/java/com/back_end_Journey/back_end_Journey/controller/ProductoController.java
+++ src/main/java/com/back_end_Journey/back_end_Journey/controller/ProductoController.java
@@ -1,12 +1,23 @@
 package com.back_end_Journey.back_end_Journey.controller;
 
+import com.back_end_Journey.back_end_Journey.model.Producto;
+import com.back_end_Journey.back_end_Journey.service.ProductoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/productos")
 public class ProductoController {
+    private final ProductoService productoService;
 
-
-
-
-
-
-
-
+    public ProductoController(ProductoService productoService) {
+        this.productoService = productoService;
+    }
+    
+    @GetMapping("/ObtenerProductos")
+    public List<Producto> listaProductos(){
+        return productoService.obtenerTodos();
+    }
 }
--- src/main/java/com/back_end_Journey/back_end_Journey/service/IProductoService.java
+++ src/main/java/com/back_end_Journey/back_end_Journey/service/IProductoService.java
@@ -1,4 +1,14 @@
 package com.back_end_Journey.back_end_Journey.service;
 
+
+import com.back_end_Journey.back_end_Journey.model.Producto;
+
+import java.util.List;
+
 public interface IProductoService {
+    List<Producto> obtenerTodos();
+    Producto obtenerPorId(Long id);
+    void guardarProducto(Producto producto);
+    void deleteProducto(Long id);
+    void updateProducto(Long id, Producto productoActualizado);
 }
