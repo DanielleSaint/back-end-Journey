ab688902b1c8596e783ba10c46846956dd97a85b
DanielleSaint <daniellesaint09@gmail.com>
2025-06-09 12:37:04 -0500

    webconfig

---
+++ src/main/java/com/back_end_Journey/back_end_Journey/jwt/WebConfig.java
@@ -0,0 +1,18 @@
+package com.back_end_Journey.back_end_Journey.jwt;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500") //verificar
+//                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
